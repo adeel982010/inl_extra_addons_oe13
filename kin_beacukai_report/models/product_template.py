59503542e6bddc268626646c4f6709e3a391e535
Kikin Kusumah <kikin.kusumah@gmail.com>
2020-09-16 13:34:24 +0700

    product code required and unique

---
+++ kin_beacukai_report/models/product_template.py
@@ -0,0 +1,18 @@
+import logging
+
+from odoo import api, fields, models, tools, _, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    _sql_constraints = [
+        ('default_code_uniq', 'unique (default_code)',
+         'The Product Code of the product must be unique!')
+    ]
+
+    default_code = fields.Char(
+        'Product Code', compute='_compute_default_code',
+        inverse='_set_default_code', store=True, required=True)
